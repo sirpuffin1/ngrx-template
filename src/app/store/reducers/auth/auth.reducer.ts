5cabc92390dccf6b53ca40c7fe4ac5af871e5303
sirpuffin1 <angelcruz981@yahoo.com>
2021-09-16 15:59:20 -0700

    create actions

--- src/app/store/reducers/auth/auth.reducer.ts
+++ src/app/store/reducers/auth/auth.reducer.ts
@@ -1,14 +1,15 @@
 import { Action, createReducer, on } from '@ngrx/store';
+import { User } from 'src/app/models/user';
 
 
 export const authFeatureKey = 'auth';
 
 export interface State {
-
+  loggedInUser: User | null;
 }
 
 export const initialState: State = {
-
+  loggedInUser: null
 };
 
 
--- src/app/store/reducers/user/user.reducer.ts
+++ src/app/store/reducers/user/user.reducer.ts
@@ -1,14 +1,15 @@
 import { Action, createReducer, on } from '@ngrx/store';
+import { User } from 'src/app/models/user';
 
 
 export const userFeatureKey = 'user';
 
 export interface State {
-
+  users: User[];
 }
 
 export const initialState: State = {
-
+  users: []
 };
 
 
