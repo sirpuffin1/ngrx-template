7a308d191d2585658800bdc387f886f1b4398482
sirpuffin1 <angelcruz981@yahoo.com>
2021-09-16 15:52:39 -0700

    create and define auth and user classes

---
+++ src/app/models/auth-payload.spec.ts
@@ -0,0 +1,7 @@
+import { AuthPayload } from './auth-payload';
+
+describe('AuthPayload', () => {
+  it('should create an instance', () => {
+    expect(new AuthPayload()).toBeTruthy();
+  });
+});
---
+++ src/app/models/auth-payload.ts
@@ -0,0 +1,6 @@
+export class AuthPayload {
+  constructor(
+    public username: string,
+    public password: string,
+  ){}
+}
